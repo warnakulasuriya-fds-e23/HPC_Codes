b035bc33897a6f11f85b0991e0c7c85fafe5f2fb
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2026-01-09 08:58:58 +0530

    [Day3] barrier

---
+++ 2026-1-9/example8.c
@@ -0,0 +1,16 @@
+//Synchronization: Atomic Operation
+#include <stdio.h>
+#include <omp.h>
+
+int main(){
+    #pragma omp parallel num_threads(4) 
+    {
+            printf("Thread %d BEFORE barier.\n", omp_get_thread_num());
+
+            #pragma omp barrier
+
+            printf("Thread %d AFTER barier.\n", omp_get_thread_num());
+       
+    } 
+    return 0;
+}
