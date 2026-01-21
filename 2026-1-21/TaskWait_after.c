e9596ab8fe01ab40423bbab4627a225444b7cb08
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2026-01-21 15:38:41 +0530

    [Day4] taskwait example

---
+++ 2026-1-21/TaskWait_after.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int instructions = 0;
+    int a=0, b=0;
+    omp_set_num_threads(4);
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp task
+            {
+                printf("Task 1 Working...\n");
+                a=10;
+                printf("a value is : %d \n",a);
+            }
+
+            #pragma omp task
+            {
+                printf("Task 2 Working...\n");
+                b=20;
+                printf("b value is : %d \n",b);
+            }
+            
+            #pragma omp taskwait
+            // Waits for both results
+
+            //Safe to execute now
+            printf("Sum = %d\n",a+b);
+        }
+    }
+    return 0;
+}
